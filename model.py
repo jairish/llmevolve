b658cd3c0489be1ef4adac5e08dc2c291b4b4765
jairish <jairish@users.noreply.github.com>
2025-07-27 18:37:59 +0000

    Initial commit: Add base chat model and training scripts, and prepare for model saving

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed, Input, Dropout
+from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed, Input
 from tensorflow.keras.models import Model
 
 # These constants must match what's used in train.py
@@ -13,25 +13,17 @@ def create_model():
     # Encoder
     encoder_inputs = Input(shape=(MAX_SEQUENCE_LENGTH,))
     encoder_embedding = Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True)(encoder_inputs) # mask_zero for padding
-    encoder_embedding = Dropout(0.3)(encoder_embedding)
-    encoder_lstm1 = LSTM(LSTM_UNITS, return_sequences=True, return_state=True)
-    encoder_outputs1, state_h1, state_c1 = encoder_lstm1(encoder_embedding)
-    encoder_lstm2 = LSTM(LSTM_UNITS, return_state=True)
-    encoder_outputs, state_h, state_c = encoder_lstm2(encoder_outputs1)
+    encoder_lstm = LSTM(LSTM_UNITS, return_state=True)
+    encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
     encoder_states = [state_h, state_c]
 
     # Decoder
     decoder_inputs = Input(shape=(MAX_SEQUENCE_LENGTH,))
     decoder_embedding = Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True)(decoder_inputs) # mask_zero for padding
-    decoder_embedding = Dropout(0.3)(decoder_embedding)
-    decoder_lstm1 = LSTM(LSTM_UNITS, return_sequences=True, return_state=True)
-    decoder_outputs1, _, _ = decoder_lstm1(decoder_embedding, initial_state=encoder_states)
-    decoder_outputs1 = Dropout(0.3)(decoder_outputs1)
-    decoder_lstm2 = LSTM(LSTM_UNITS, return_sequences=True, return_state=False)  # Removed return_state
-    decoder_outputs2 = decoder_lstm2(decoder_outputs1)
-    decoder_outputs2 = Dropout(0.3)(decoder_outputs2)
+    decoder_lstm = LSTM(LSTM_UNITS, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
     decoder_dense = TimeDistributed(Dense(VOCAB_SIZE, activation='softmax'))
-    output = decoder_dense(decoder_outputs2)
+    output = decoder_dense(decoder_outputs)
 
     model = Model([encoder_inputs, decoder_inputs], output)
 
