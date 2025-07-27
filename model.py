f224e0651f168f8b243f072989759a64372977e4
jairish <jairish@users.noreply.github.com>
2025-07-27 18:38:37 +0000

    Evolution Cycle 3: Loss inf

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed, Input, Dropout
+from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed, Input, Dropout, LayerNormalization
 from tensorflow.keras.models import Model
 
 # These constants must match what's used in train.py
@@ -13,18 +13,18 @@ def create_model():
     # Encoder
     encoder_inputs = Input(shape=(MAX_SEQUENCE_LENGTH,))
     encoder_embedding = Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True)(encoder_inputs) # mask_zero for padding
-    encoder_embedding = Dropout(0.3)(encoder_embedding)
+    encoder_embedding = LayerNormalization()(encoder_embedding)
     encoder_lstm1 = LSTM(LSTM_UNITS, return_sequences=True, return_state=True)
     encoder_outputs1, state_h1, state_c1 = encoder_lstm1(encoder_embedding)
-    encoder_lstm1_dropout = Dropout(0.3)(encoder_outputs1)
-    encoder_lstm2 = LSTM(LSTM_UNITS, return_state=True)
-    encoder_outputs, state_h, state_c = encoder_lstm2(encoder_lstm1_dropout)
+    encoder_outputs1 = Dropout(0.3)(encoder_outputs1)
+    encoder_lstm2 = LSTM(LSTM_UNITS, return_sequences=False, return_state=True)
+    encoder_outputs, state_h, state_c = encoder_lstm2(encoder_outputs1)
     encoder_states = [state_h, state_c]
 
     # Decoder
     decoder_inputs = Input(shape=(MAX_SEQUENCE_LENGTH,))
     decoder_embedding = Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True)(decoder_inputs) # mask_zero for padding
-    decoder_embedding = Dropout(0.3)(decoder_embedding)
+    decoder_embedding = LayerNormalization()(decoder_embedding)
     decoder_lstm = LSTM(LSTM_UNITS, return_sequences=True, return_state=True)
     decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
     decoder_outputs = Dropout(0.3)(decoder_outputs)
